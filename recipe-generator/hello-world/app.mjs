692ed3aa2f247c6d3aaa9b9c949e2e30b4b6cdf4
John Staudt <johnstaudt.sa@gmail.com>
2025-02-16 12:09:13 -0600

    added logic for randomization and handling queries that return less than 4 recipes

--- recipe-generator/hello-world/app.mjs
+++ recipe-generator/hello-world/app.mjs
@@ -19,11 +19,59 @@ const knex = Knex({
 });
 
 export const lambdaHandler = async (event, context) => {
-  const recipes = await knex.select().table("recipeList");
+  let remainderRecipes = [];
+  let updated = [];
+
+  const getNewRecipes = async (limit) => {
+    const recipes = await knex("recipeList").where("hasBeenMade", "N");
+
+    return recipes
+      .sort(() => Math.random() - 0.5)
+      .slice(0, limit)
+      .map((item, index) => {
+        return item.Name;
+      });
+  };
+
+  const updateRecipes = async () => {
+    updated = await knex("recipeList").whereIn("Name", randomRecipes).update({
+      hasBeenMade: "Y",
+    });
+
+    return updated;
+  };
+
+  let randomRecipes = await getNewRecipes(4);
+  const randomRecipeCount = randomRecipes.length;
+
+  // if there are less than 4 recipes retrieved
+  if (randomRecipeCount < 4) {
+    // update all records to hasBeenMade=N
+    await knex("recipeList").update({
+      hasBeenMade: "N",
+    });
+
+    // if there is at least 1 recipe retrieved, get remainder recipes and merge arrays
+    if (randomRecipeCount > 0) {
+      remainderRecipes = await getNewRecipes(4 - randomRecipeCount);
+      randomRecipes = randomRecipes.concat(remainderRecipes);
+
+      await updateRecipes(randomRecipes);
+    } else {
+      // if there are no recipes retrieved, get a fresh 4 and update them to hasBeenMade=Y
+      randomRecipes = await getNewRecipes(4);
+
+      await updateRecipes(randomRecipes);
+    }
+  } else {
+    // if all 4 recipes are retrieved, update those 4 recipes to hasBeenMade=Y
+    await updateRecipes(randomRecipes);
+  }
+
   const response = {
     statusCode: 200,
     body: {
-      message: recipes,
+      message: randomRecipes,
     },
   };
 
