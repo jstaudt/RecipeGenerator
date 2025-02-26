9ece9c3a6d3247f7e7f1f3de891d0d754a884da8
John Staudt <johnstaudt.sa@gmail.com>
2025-02-26 12:50:03 -0600

    added aws ses send command, formatting, comments

--- recipe-generator/hello-world/app.mjs
+++ recipe-generator/hello-world/app.mjs
@@ -1,6 +1,8 @@
 import Knex from "knex";
 import "dotenv/config.js";
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
+const sesClient = new SESClient({ region: "us-east-1" });
 const host = process.env.HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
@@ -19,11 +21,16 @@ const knex = Knex({
 });
 
 export const lambdaHandler = async (event, context) => {
+  let randomRecipes = [];
   let remainderRecipes = [];
-  let updated = [];
 
+  /** gets 4 or less recipes at random from all recipes that have not been made
+   * @param {number} limit - the number of recipes to return (<= 4)
+   */
   const getNewRecipes = async (limit) => {
-    const recipes = await knex("recipeList").where("hasBeenMade", "N");
+    const recipes = await knex("recipeList")
+      .where("hasBeenMade", "N")
+      .whereNotIn("Name", randomRecipes);
 
     return recipes
       .sort(() => Math.random() - 0.5)
@@ -33,15 +40,15 @@ export const lambdaHandler = async (event, context) => {
       });
   };
 
+  /** updates all recipes that are stored in randomRecipes array to hasBeenMade=Y
+   */
   const updateRecipes = async () => {
-    updated = await knex("recipeList").whereIn("Name", randomRecipes).update({
+    return await knex("recipeList").whereIn("Name", randomRecipes).update({
       hasBeenMade: "Y",
     });
-
-    return updated;
   };
 
-  let randomRecipes = await getNewRecipes(4);
+  randomRecipes = await getNewRecipes(4);
   const randomRecipeCount = randomRecipes.length;
 
   // if there are less than 4 recipes retrieved
@@ -51,21 +58,48 @@ export const lambdaHandler = async (event, context) => {
       hasBeenMade: "N",
     });
 
-    // if there is at least 1 recipe retrieved, get remainder recipes and merge arrays
+    // if there is at least 1 recipe retrieved, get remainder recipes, merge remainder and random recipes, and update the 4 corresponding records to haveBeenMade=Y
     if (randomRecipeCount > 0) {
       remainderRecipes = await getNewRecipes(4 - randomRecipeCount);
       randomRecipes = randomRecipes.concat(remainderRecipes);
 
-      await updateRecipes(randomRecipes);
+      await updateRecipes();
     } else {
       // if there are no recipes retrieved, get a fresh 4 and update them to hasBeenMade=Y
       randomRecipes = await getNewRecipes(4);
 
-      await updateRecipes(randomRecipes);
+      await updateRecipes();
     }
   } else {
     // if all 4 recipes are retrieved, update those 4 recipes to hasBeenMade=Y
-    await updateRecipes(randomRecipes);
+    await updateRecipes();
+  }
+
+  const command = new SendEmailCommand({
+    Destination: {
+      ToAddresses: ["johnstaudt.sa@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: JSON.stringify(randomRecipes),
+          Charset: "UTF-8",
+        },
+      },
+      Subject: {
+        Data: "From Johnny: ",
+        Charset: "UTF-8",
+      },
+    },
+    Source: "johnstaudt.sa@gmail.com",
+  });
+
+  try {
+    await sesClient.send(command);
+  } catch (error) {
+    console.log("send email error");
+  } finally {
+    console.log("send email success");
   }
 
   const response = {
